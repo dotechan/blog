e6575206a8c907d8073fb970612e384ab256a922
ito-hi <this.is.itoooo.0120@outlook.jp>
2020-04-28 23:04:18 +0900

    Fixed to include draft content in build targets.

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # If a command fails then the deploy stops
-set -e
+set -Ceu
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -D # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
